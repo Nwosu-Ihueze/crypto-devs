991caa68c903b782cd924d4843567550eb17b497
Nwosu-Ihueze <nwosunneomagmail.com>
2022-01-15 22:13:36 -0500

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xab3879eb3B6a5bc45CfE27a28C653ec340f72279";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
